a287d8d9b92d76d1881f7ec3eb3a4538b59b1741
Santiago <santiago.garcia1999@hotmail.com>
2020-08-12 16:57:23 -0500

    Started class for matrix transformations

---
+++ PROJECTS/BASICS/transformation.py
@@ -0,0 +1,31 @@
+# ROBOTICS
+# MODULE TO APPLY A GENERAL TRANSFORMATION HOMOGENEOUS MATRIX
+# Santiago Garcia Arango
+
+import numpy as np
+import math
+
+class Transformation:
+    def __init__(self):
+        pass
+
+
+
+
+# angle = math.radians(30)
+
+# AB_transformation = np.array([
+#     [math.cos(angle), -math.sin(angle), 0, 10],
+#     [math.sin(angle), math.cos(angle), 0, 5],
+#     [0, 0, 1, 0],
+#     [0, 0, 0, 1],
+# ])
+
+# print(AB_transformation)
+
+# B_PA = np.array([3,7,0,1])
+
+# A_P = np.dot(AB_transformation, B_PA.transpose())
+
+
+# print(A_P)
