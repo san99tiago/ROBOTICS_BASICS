860998624665400839e12dd0a0b742ee90159aeb
Santiago <santiago.garcia1999@hotmail.com>
2020-08-18 23:04:04 -0500

    Added function explanation and add general Transformation_Matrix result as a self instance

--- PROJECTS/BASICS/transformation.py
+++ PROJECTS/BASICS/transformation.py
@@ -5,22 +5,29 @@ import numpy as np
 import math
 
 class Transformation:
+    """
+    TRANSFORMATION MATRICES FOR ROBOTICS. Santiago Garcia Arango.
+    angle_x = rotation of {B} with respect of {A} in "x" axis.
+    angle_y = rotation of {B} with respect of {A} in "y" axis.
+    angle_z = rotation of {B} with respect of {A} in "z" axis.
+    vector_AB = vector from origin of {A} to origin of {B}.
+    vector_BP = vector from origin of {B} to point "P".
+    """
     def __init__(self, angle_x, angle_y, angle_z, vector_AB, vector_BP):
         # Add extra "1" to vector_AB
         vector_AB.append(1)
-        self.vector_AB = np.array(vector_AB)
         self.vector_AB = np.transpose([vector_AB])
+        print("\nvector_AB:\n", self.vector_AB)
 
-        print(self.vector_AB)
+        # Add extra "1" to vector_BP
+        vector_BP.append(1)
+        self.vector_BP = np.transpose([vector_BP])
+        print("\nvector_BP:\n", self.vector_BP)
 
         # Initialize rotation matrices in zero
         self.RX = np.eye(3)
         self.RY = np.eye(3)
         self.RZ = np.eye(3)
-
-        print("\nRX:\n", self.RX)
-        print("\nRY:\n", self.RY)
-        print("\nRZ:\n", self.RZ)
 
         # Change the rotation matrices based on inputs
         self.create_rotation_matrix("x", angle_x)
@@ -34,27 +41,33 @@ class Transformation:
         # Create homogeneous transformation matrix
         self.create_transformation_matriz()
 
+        # Find vector_AP (from main coordinate system to point B)
+        self.vector_AP = np.dot(self.TM, self.vector_BP)
+
 
     def create_rotation_matrix(self, axis, angle):
         angle = math.radians(angle)  # Convert from degrees to radians
+
+        if axis == "x":
+            self.RX = np.array([
+                [1, 0, 0],
+                [0, math.cos(angle), -math.sin(angle)],
+                [0, math.sin(angle), math.cos(angle)]
+            ])
+
+        if axis == "y":
+            self.RY = np.array([
+                [math.cos(angle), 0, math.sin(angle)],
+                [0, 1, 0],
+                [-math.sin(angle), 0, math.cos(angle)]
+            ])
+
         if axis == "z":
             self.RZ = np.array([
                 [math.cos(angle), -math.sin(angle), 0],
                 [math.sin(angle), math.cos(angle), 0],
                 [0, 0, 1]
             ])
-        # if axis == "z":
-        #     self.RX = np.array[
-        #         [math.cos(angle), -math.sin(angle), 0]
-        #         [math.sin(angle), math.cos(angle), 0]
-        #         [0, 0, 1]
-        #     ]
-        # if axis == "z":
-        #     self.RX = np.array[
-        #         [math.cos(angle), -math.sin(angle), 0]
-        #         [math.sin(angle), math.cos(angle), 0]
-        #         [0, 0, 1]
-        #     ]
     
     def create_transformation_matriz(self):
         R_ZYX = np.dot(np.dot(self.RZ, self.RY), self.RX)
@@ -71,27 +84,6 @@ class Transformation:
 # TESTS
 if __name__ == "__main__":
     test = Transformation(0, 0, 30, [10, 5, 0], [3, 7, 0])
+    print("\nvector_AP:\n", test.vector_AP)
 
 
-
-
-
-
-
-# angle = math.radians(30)
-
-# AB_transformation = np.array([
-#     [math.cos(angle), -math.sin(angle), 0, 10],
-#     [math.sin(angle), math.cos(angle), 0, 5],
-#     [0, 0, 1, 0],
-#     [0, 0, 0, 1],
-# ])
-
-# print(AB_transformation)
-
-# B_PA = np.array([3,7,0,1])
-
-# A_P = np.dot(AB_transformation, B_PA.transpose())
-
-
-# print(A_P)
