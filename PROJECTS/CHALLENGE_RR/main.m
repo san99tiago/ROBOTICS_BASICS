a0a24a1f0b2aeda2a683800022748da2eae9966e
Santiago <santiago.garcia1999@hotmail.com>
2020-09-04 23:05:49 -0500

    Started robot R-R arm challenge

---
+++ PROJECTS/CHALLENGE_RR/main.m
@@ -0,0 +1,24 @@
+% CHALLENGE: DRAW A ROBOTIC RR ARM AND DO THE TCD AND TCI
+
+% First position
+x_inicial = 2;
+y_inicial = 5;
+
+% Second position
+x_final = 8;
+y_final = 7;
+
+% Measurements of L1 and L2
+L1 = 6;
+L2 = 4;
+
+
+figure()
+L = sqrt(0);
+plot([0, x1], [0, y1], "LineWidth", 2);
+xlim([-10, 10]);
+ylim([-10, 10]);
+GRAFICA_BONITA("EL MEJOR ROBOT RR");
+
+
+
