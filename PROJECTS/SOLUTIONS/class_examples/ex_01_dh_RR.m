8779f3117fb729bc561e3e299f0ec9b097c43de0
Santiago <santiago.garcia1999@hotmail.com>
2020-09-04 23:21:12 -0500

    Added RR class example of Denavit Hartenberg solution

---
+++ PROJECTS/SOLUTIONS/class_examples/ex_01_dh_RR.m
@@ -0,0 +1,31 @@
+% TEST DH PARAMETERS TABLE FOR AN "R-R" ROBOT
+% Santiago Garcia Arango
+
+% Access upper folder functions
+addpath(genpath("../"));
+
+% Define Symbolic variables and assume they are real (avoid "complex")
+syms theta1 L1 theta2 L2
+assume(theta1, "positive")
+assume(L1, "positive")
+assume(theta2, "positive")
+assume(L2, "positive")
+
+
+% Define DH table of parameters
+DH_TABLE = [0,  0, 0, theta1;
+            0, L1, 0, theta2;
+            0, L2, 0,      0]
+
+% Main functions to obtain TM and values for position and rotation
+[TM, cell_of_TM] = denavit_hartenberg(DH_TABLE);
+[vector_K, angle, P] = inv_transformation(TM);
+
+
+% We show the simplified version of final TM, position and rotation
+% "simplify" lets us show the simplest version of symbolic expression
+TM = simplify(TM, 'IgnoreAnalyticConstraints', true)
+vector_K = simplify(vector_K, 'IgnoreAnalyticConstraints',true)
+angle = simplify(angle,'IgnoreAnalyticConstraints',true)
+P = simplify(P,'IgnoreAnalyticConstraints',true)
+
