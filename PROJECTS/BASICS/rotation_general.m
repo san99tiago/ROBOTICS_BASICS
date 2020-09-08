fccc527314a5ffe4b4c996bc8e11130c8acecfa8
Santiago <santiago.garcia1999@hotmail.com>
2020-09-08 09:14:59 -0500

    Updated to have notion of <fixed> and <euler> angles

--- PROJECTS/BASICS/rotation_general.m
+++ PROJECTS/BASICS/rotation_general.m
@@ -1,11 +1,16 @@
-% ROBOTICS: OBTAIN RESULTING R_XYZ MATRIX WITH RX, RY, RZ
+% ROBOTICS: GET ROTATION MATRIX FROM FIXED AND RELATIVE ANGLES
 % SANTIAGO GARCIA ARANGO
-clear
+clear variables; clc;
 
 syms gama beta alfa
 
+% Main three rotations
 RX = [1, 0, 0; 0, cos(gama), -sin(gama); 0, sin(gama), cos(gama)]
 RY = [cos(beta), 0, sin(beta); 0, 1, 0; -sin(beta), 0, cos(beta)]
 RZ = [cos(alfa), -sin(alfa), 0; sin(alfa), cos(alfa), 0; 0, 0, 1]
 
+% Fixed angles
 RXYZ = RZ*RY*RX
+
+% Euler angles
+RXYZ = RX*RY*RZ
