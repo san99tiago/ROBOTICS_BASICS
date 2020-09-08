e93abb2aaf735ec2c26ca712e4615ffbd0bc3b83
Santiago <santiago.garcia1999@hotmail.com>
2020-09-08 09:15:18 -0500

    Added main script to test symbolic matrices with python

--- PROJECTS/BASICS/test_symbolic_matrix.py
+++ PROJECTS/BASICS/test_symbolic_matrix.py
@@ -7,15 +7,21 @@ import rotacion_equivalente_sym
 theta, phi = sympy.symbols("theta phi")
 
 
-vector_K = [1, 0, 0]
+vector_K = [1, 1, 0]
 angle = phi
 test = rotacion_equivalente_sym.RotacionEquivalente(vector_K, angle)
 
 print(test.Rk)
 print(np.shape(test.Rk))
 
+
+print( np.dot(test.Rk, test.Rk))
+
+np.save("TEST_NP_SAVE.csv", test.Rk)
+
 for i in range(np.shape(test.Rk)[0]):
     for j in range(np.shape(test.Rk)[1]):
-        test.Rk[i, j] = test.Rk[i, j].subs([(angle, 0)])
+        test.Rk[i, j] = test.Rk[i, j].subs([(angle, 45)]).evalf()
 
 print(test.Rk)
+
