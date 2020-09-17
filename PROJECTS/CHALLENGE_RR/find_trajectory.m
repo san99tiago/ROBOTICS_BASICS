53819cb5b4c6cc67ed6b40d015a73873fb969b68
Santiago <santiago.garcia1999@hotmail.com>
2020-09-16 23:17:20 -0500

    Added possibility for fifth order trajectories (not finished yet)

--- PROJECTS/CHALLENGE_RR/find_trajectory.m
+++ PROJECTS/CHALLENGE_RR/find_trajectory.m
@@ -1,10 +1,11 @@
+% FIND_TRAJECTORY: find resulting trajectory inbetween two points
+% SANTIAGO GARCIA ARANGO
 
 
 function [x, y] = find_trajectory(trajectory_type, x_inicial, x_final,...
                                     y_inicial, y_final, SPEED_POINTS)
 
     if trajectory_type == "linear"
-        %FIND_TRAJECTORY: find resulting trajectory inbetween two points
         % Find line equation of the robot
         m = (y_final - y_inicial)/(x_final - x_inicial);
         b = y_final - m*x_final;
@@ -14,6 +15,15 @@ function [x, y] = find_trajectory(trajectory_type, x_inicial, x_final,...
         y = m*x + b;
     end
 
+    
+    if trajectory_type == "5th"
+        % Define the intitial conditions for the fifth order trajectory
+        x_0 = 0;
+        y_0 = 0;
+        % MUST FINISH
+    end
+
+    
     
 end
 
