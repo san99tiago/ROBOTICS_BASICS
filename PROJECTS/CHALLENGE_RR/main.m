7856e81a94e265acdae1fb17133717e6995e9b43
Santiago <santiago.garcia1999@hotmail.com>
2020-09-17 02:49:38 -0500

    Updated main script with new functions to plot main variables

--- PROJECTS/CHALLENGE_RR/main.m
+++ PROJECTS/CHALLENGE_RR/main.m
@@ -4,7 +4,7 @@
 % Access the point A and point B for the trajectories
 x = inputdlg({'X inicial','Y inicial','X final', 'Y final'}, ...
               'Santiago Garcia', [1 50; 1 50; 1 50; 1 50], ...
-              {'-10','3','8','5'}); 
+              {'5','4','5','-4'}); 
           
 % Initial and Final Position based on user input
 x_inicial = str2double(x{1});
@@ -13,14 +13,21 @@ x_final = str2double(x{3});
 y_final = str2double(x{4});
 
 % Measurements of L1 and L2
-L1 = 8;
-L2 = 7;
+L1 = 6;
+L2 = 4;
 
 % Define the amout of points for the vector of point in trajectory
-SPEED_POINTS = 300;
+TIME = 10;
 
-[x, y] = find_trajectory("5th", x_inicial, x_final, y_inicial, ...
-                        y_final, SPEED_POINTS);
+[t, x, y, vel_x, vel_y, ace_x, ace_y] = find_trajectory("5th", ...
+                    x_inicial, x_final, y_inicial, y_final, TIME);
 
                     
-animate_trajectory(x, y, L1, L2, SPEED_POINTS);
+animate_trajectory(x, y, L1, L2);
+
+
+graph_positions(t, x, y);
+graph_velocities(t, vel_x, vel_y);
+graph_acelerations(t, ace_x, ace_y);
+
+
