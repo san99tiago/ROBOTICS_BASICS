b4ce80fa39609587eaf1644d5d0a31f53cfb6e83
Santiago <santiago.garcia1999@hotmail.com>
2020-09-17 02:51:44 -0500

    Included new circular plot to the robot arm to see maximum limits of ROI

--- PROJECTS/CHALLENGE_RR/animate_trajectory.m
+++ PROJECTS/CHALLENGE_RR/animate_trajectory.m
@@ -4,25 +4,37 @@
 % Parameters:
 % --> x: vector of all "x" positions of the vector
 % --> y: vector of all "y" positions of the vector
-% --> SPEED_POINTS: number of points to track
 % --> L1: length of first robot arm
 % --> L2: lenght of second robot arm
 
-function [] = animate_trajectory(x, y, L1, L2, SPEED_POINTS)
+function [] = animate_trajectory(x, y, L1, L2)
     % Start the figure animation
     figure(1)
+    
+    % Create a circle for the R.O.I (maximum one)
+    r = L1 + L2;
+    x_origin =0;
+    y_origin=0;
+    theta = linspace(0,2*pi,100);
+    plot(x_origin + r*cos(theta),y_origin + r*sin(theta), 'r.');
+    hold on 
+    
 
     % Plot main line to follow
     plot(x,y, "r", "LineWidth", 0.2);
+    hold on
 
     % Button to exit if needed
     EXIT = uicontrol('Style', 'PushButton', ...
-                        'String', 'SALIRSE', ...
+                        'String', 'CLOSE', ...
                         'Callback', 'delete(gcbf)');
 
+    % Get amount of points to be plotted
+    POINTS = size(x, 2);
 
-
-    for i=1:SPEED_POINTS
+    % Main loop to plot the trajectory based on Inverse Kinematics
+    for i=1:POINTS
+       
         gamma = atan2(y(i), x(i));
         LT = sqrt(x(i)^2 + y(i)^2);
         alfa = acos((LT^2 + L1^2 - L2^2)/(2*L1*LT));
@@ -40,12 +52,12 @@ function [] = animate_trajectory(x, y, L1, L2, SPEED_POINTS)
 
         xlim([-10, 10]);
         ylim([-10, 10]);
-        pretty_figure("EL MEJOR ROBOT RR");
+        pretty_figure("SANTIAGO GARCIA'S ROBOT ARM");
 
         pause(1/1000);
 
         % Allows us to keep showing robot arm at the last point
-        if i ~= SPEED_POINTS
+        if i ~= POINTS
             delete(b1);
             delete(b2);
         end
