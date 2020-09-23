2a2576b34ae7e7394916a901779b954d83370de1
Santiago <santiago.garcia1999@hotmail.com>
2020-09-22 22:40:30 -0500

    Added inverse kinematics for both baxter arms (included right arme with its matrices)

--- PROJECTS/CHALLENGE_BAXTER/inverse_kinematics_baxter.m
+++ PROJECTS/CHALLENGE_BAXTER/inverse_kinematics_baxter.m
@@ -3,7 +3,8 @@
 
 
 
-function [THETAS] = inverse_kinematics_baxter(TM_W0_GL)
+
+function [THETAS] = inverse_kinematics_baxter(TM_W0_Gtool, arm_side)
 
 
 % Remark: remember that theta3=0 and L5=0
@@ -49,57 +50,119 @@ TM_7_GR = TM_7_GL;
 
 
 
-
-% Understanding the corresponding TM from the article, this can be solved:
-TM_0_6 = (TM_W0_BL * TM_BL_0)^(-1) * TM_W0_GL * (TM_7_GL)^(-1)
-
-
-% Find theta1
-PX_0_4 = TM_0_6(1, 4);
-PY_0_4 = TM_0_6(2, 4);
-
-theta1 = atan2(PY_0_4, PX_0_4)
+if arm_side == "left"
+    % Understanding the corresponding TM from the article, this can be solved:
+    TM_0_6 = (TM_W0_BL * TM_BL_0)^(-1) * TM_W0_Gtool * (TM_7_GR)^(-1)
 
 
-% Find theta2
-PZ_0_4 = TM_0_6(3, 4);
-E = 2*LH*(L1 - PX_0_4/cos(theta1));
-F = 2*LH*PZ_0_4;
-G = (PX_0_4^2)/((cos(theta1))^2) + L1^2 + LH^2 - L4^2 + PZ_0_4^2 - 2*L1*PX_0_4/cos(theta1);
+    % Find theta1
+    PX_0_4 = TM_0_6(1, 4);
+    PY_0_4 = TM_0_6(2, 4);
 
-t1 = (-F + sqrt(E^2 + F^2 - G^2))/(G - E);
-
-theta2 = 2*atan(t1)
+    theta1 = atan2(PY_0_4, PX_0_4)
 
 
-% Find theta4
-theta4 = atan2(-PZ_0_4 - LH*sin(theta2), PX_0_4/cos(theta1) - L1 - LH*cos(theta2)) - theta2
+    % Find theta2
+    PZ_0_4 = TM_0_6(3, 4);
+    E = 2*LH*(L1 - PX_0_4/cos(theta1));
+    F = 2*LH*PZ_0_4;
+    G = (PX_0_4^2)/((cos(theta1))^2) + L1^2 + LH^2 - L4^2 + PZ_0_4^2 - 2*L1*PX_0_4/cos(theta1);
+
+    t1 = (-F + sqrt(E^2 + F^2 - G^2))/(G - E);
+
+    theta2 = 2*atan(t1)
+
+    % We make only use the real part (img is not significant most of the times)
+    theta2 = real(theta2);
 
 
-% Find theta5
-DH_0_3 = [    0,  0,  0,        theta1;
-          -pi/2, L1,  0,        theta2;
-              0, LH,  0, theta4 + pi/2];
-              
-TM_0_3 = denavit_hartenberg(DH_0_3);
-R_0_3 = TM_0_3(1:3, 1:3);
-
-R_0_6 = TM_0_6(1:3, 1:3);
-
-R_3_6 = (R_0_3)^(-1) * R_0_6;
-
-theta5 = atan2(R_3_6(3,3), R_3_6(1,3))
+    % Find theta4
+    theta4 = atan2(-PZ_0_4 - LH*sin(theta2), PX_0_4/cos(theta1) - L1 - LH*cos(theta2)) - theta2
 
 
-% Find theta7
-theta7 = atan2(-R_3_6(2,2), R_3_6(2,1))
+    % Find theta5
+    DH_0_3 = [    0,  0,  0,        theta1;
+              -pi/2, L1,  0,        theta2;
+                  0, LH,  0, theta4 + pi/2];
+
+    TM_0_3 = denavit_hartenberg(DH_0_3, 0);
+    R_0_3 = TM_0_3(1:3, 1:3);
+
+    R_0_6 = TM_0_6(1:3, 1:3);
+
+    R_3_6 = (R_0_3)^(-1) * R_0_6;
+
+    theta5 = atan2(R_3_6(3,3), R_3_6(1,3));
 
 
-% Find theta6
-theta6 = atan2(R_3_6(2,1)/cos(theta7), -R_3_6(2,3))
+    % Find theta7
+    theta7 = atan2(-R_3_6(2,2), R_3_6(2,1));
 
 
-THETAS = [theta1, theta2, theta4, theta5, theta6, theta7];
+    % Find theta6
+    theta6 = atan2(R_3_6(2,1)/cos(theta7), -R_3_6(2,3));
+
+
+    THETAS = [theta1, theta2, theta4, theta5, theta6, theta7];
+end
+
+
+if arm_side == "right"
+    % Understanding the corresponding TM from the article, this can be solved:
+    TM_0_6 = (TM_W0_BR * TM_BL_0)^(-1) * TM_W0_Gtool * (TM_7_GL)^(-1)
+
+
+    % Find theta1
+    PX_0_4 = TM_0_6(1, 4);
+    PY_0_4 = TM_0_6(2, 4);
+
+    theta1 = atan2(PY_0_4, PX_0_4)
+
+
+    % Find theta2
+    PZ_0_4 = TM_0_6(3, 4);
+    E = 2*LH*(L1 - PX_0_4/cos(theta1));
+    F = 2*LH*PZ_0_4;
+    G = (PX_0_4^2)/((cos(theta1))^2) + L1^2 + LH^2 - L4^2 + PZ_0_4^2 - 2*L1*PX_0_4/cos(theta1);
+
+    t1 = (-F + sqrt(E^2 + F^2 - G^2))/(G - E);
+
+    theta2 = 2*atan(t1)
+
+    % We make only use the real part (img is not significant most of the times)
+    theta2 = real(theta2);
+
+
+    % Find theta4
+    theta4 = atan2(-PZ_0_4 - LH*sin(theta2), PX_0_4/cos(theta1) - L1 - LH*cos(theta2)) - theta2
+
+
+    % Find theta5
+    DH_0_3 = [    0,  0,  0,        theta1;
+              -pi/2, L1,  0,        theta2;
+                  0, LH,  0, theta4 + pi/2];
+
+    TM_0_3 = denavit_hartenberg(DH_0_3, 0);
+    R_0_3 = TM_0_3(1:3, 1:3);
+
+    R_0_6 = TM_0_6(1:3, 1:3);
+
+    R_3_6 = (R_0_3)^(-1) * R_0_6;
+
+    theta5 = atan2(R_3_6(3,3), R_3_6(1,3));
+
+
+    % Find theta7
+    theta7 = atan2(-R_3_6(2,2), R_3_6(2,1));
+
+
+    % Find theta6
+    theta6 = atan2(R_3_6(2,1)/cos(theta7), -R_3_6(2,3));
+
+
+    THETAS = [theta1, theta2, theta4, theta5, theta6, theta7];
+end
+
 
 end
 
