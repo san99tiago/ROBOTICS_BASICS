8cf1972fe6f96c8b2f73b380d0c1f1071c651c6e
Santiago <santiago.garcia1999@hotmail.com>
2020-09-22 22:42:02 -0500

    Added main file for executing the whole trajectories with a better and more detailed approach

--- PROJECTS/CHALLENGE_BAXTER/main.m
+++ PROJECTS/CHALLENGE_BAXTER/main.m
@@ -1,24 +1,10 @@
 % CHALLENGE: DRAW A ROBOTIC BAXTER ARM FROM POINT A TO POINT B
 % SANTIAGO GARCIA ARANGO
+% ------------------------------------------------------------------------
 
-% Measurements of Baxter's distances (see given document)
-L0 = 0.27035;
-L1 = 0.069;
-L2 = 0.36435;
-L3 = 0.069;
-L4 = 0.37429;
-L5 = 0.01;
-L6 = 0.36830;
-L = 0.278;
-h = 0.064;
-H = 1.104;
-LH = sqrt(L2^2 + L3^2);
-
-         
-
-%%
 % Get initial and final transformations from positions and angles (inputs)
-[TM_A, TM_B, POINT_A, POINT_B] = get_initial_tranformations();
+[TM_A_1, TM_B_1, POINT_A_1, POINT_B_1] = get_initial_tranformations_1();
+[TM_A_2, TM_B_2, POINT_A_2, POINT_B_2] = get_initial_tranformations_2();
 
 
 %%
@@ -28,23 +14,51 @@ TIME = 10;
 
 % Generate main trajectories of 5th order for POS-ANG with derivatives
 for var=1:3
+    % LEFT ARM...
     % Generate the X, Y, Z positions vectors with its derivatives
     [~, POS_1(:,var), D_POS_1(:,var),DD_POS_1(:,var)] = find_trajectory(...
-       str2double(POINT_A{var}), str2double(POINT_B{var}), TIME);
+       str2double(POINT_A_1{var}), str2double(POINT_B_1{var}), TIME);
     
    % Generate the X, Y, Z angles vectors with its derivatives
     [t, ANG_1(:,var), D_ANG_1(:,var),DD_ANG_1(:,var)] = find_trajectory(...
-       str2double(POINT_A{var+3}), str2double(POINT_B{var+3}), TIME);
+       str2double(POINT_A_1{var+3}), str2double(POINT_B_1{var+3}), TIME);
+    
+   
+   % RIGHT ARM...
+   % Generate the X, Y, Z positions vectors with its derivatives
+    [~, POS_2(:,var), D_POS_2(:,var),DD_POS_2(:,var)] = find_trajectory(...
+       str2double(POINT_A_2{var}), str2double(POINT_B_2{var}), TIME);
+    
+   % Generate the X, Y, Z angles vectors with its derivatives
+    [t, ANG_2(:,var), D_ANG_2(:,var),DD_ANG_2(:,var)] = find_trajectory(...
+       str2double(POINT_A_2{var+3}), str2double(POINT_B_2{var+3}), TIME);
 end
 
+%% 
+% Graph the animated trajectory point by point
+  
+animate_trajectory(POS_1, ANG_1, POS_2, ANG_2);
 
+%%
+% Graph the variable plots for both arms
+
+graph_positions(t, POS_1(:,1), POS_1(:,2), POS_1(:,3),...
+                    ANG_1(:,1), ANG_1(:,2), ANG_1(:,3), "left");
                     
-animate_trajectory(POS_1, ANG_1);
+graph_positions(t, POS_2(:,1), POS_2(:,2), POS_2(:,3),...
+                    ANG_2(:,1), ANG_2(:,2), ANG_2(:,3), "right");
+                    
+graph_velocities(t, D_POS_1(:,1), D_POS_1(:,2), D_POS_1(:,3),...
+                    D_ANG_1(:,1), D_ANG_1(:,2), D_ANG_1(:,3), "left");
+                    
+graph_velocities(t, D_POS_2(:,1), D_POS_2(:,2), D_POS_2(:,3),...
+                    D_ANG_2(:,1), D_ANG_2(:,2), D_ANG_2(:,3), "right");
 
+graph_acelerations(t, DD_POS_1(:,1), DD_POS_1(:,2), DD_POS_1(:,3),...
+                     DD_ANG_1(:,1), DD_ANG_1(:,2), DD_ANG_1(:,3), "left");
+                    
+graph_acelerations(t, DD_POS_2(:,1), DD_POS_2(:,2), DD_POS_2(:,3),...
+                    DD_ANG_2(:,1), DD_ANG_2(:,2), DD_ANG_2(:,3), "right");
 
-% graph_positions(t, POS_1(:,1), POS_1(:,2));
-% graph_positions(t, ANG_1(:,1), ANG_1(:,2));
-% graph_velocities(t, vel_x, vel_y);
-% graph_acelerations(t, ace_x, ace_y);
 
 
