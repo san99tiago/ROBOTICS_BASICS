d36302bdd18e5a6b838566c172b5d15ead18549b
Santiago <santiago.garcia1999@hotmail.com>
2020-11-12 23:46:12 -0500

    Added example of previous DH exam midterm

---
+++ PROJECTS/SOLUTIONS/class_examples/ex_05_RRP.m
@@ -0,0 +1,28 @@
+% TEST DH PARAMETERS TABLE FOR AN "R-R-P" ROBOT
+% Santiago Garcia Arango
+
+% Access upper folder functions
+addpath(genpath("../"));
+
+% Define Symbolic variables and assume they are real (avoid "complex")
+syms theta1 q1 q2 q3 L1
+assume(q1, "positive")
+assume(q2, "positive")
+assume(q3, "positive")
+assume(L1, "positive")
+
+
+% Define DH table of parameters
+DH_TABLE = [   0,  0, L1, pi/2 + q1;
+            pi/2,  0,  0, pi/2 + q2;
+            pi/2,  0, q3,         0;
+                0, 0,  0,         0]
+
+% Main functions to obtain TM and values for position and rotation
+[TM, cell_of_TM] = denavit_hartenberg(DH_TABLE);
+
+
+% We show the simplified version of final TM, position and rotation
+% "simplify" lets us show the simplest version of symbolic expression
+TM = simplify(TM, 'IgnoreAnalyticConstraints', true)
+
